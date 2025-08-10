a11a6d3113fcf59cc124881ae85f9748a9f9c8a5
Imran Ali Namazi <imran@amadeusweb.com>
2025-08-10 11:29:30 +0530

    home page - slider & template..

--- cms.php
+++ cms.php
@@ -1,4 +1,35 @@
 <?php
 variables([
 	'sections-have-files' => true,
+	'special-nodes' => [],
 ]);
+
+function enrichThemeVars($vars, $what) {
+	if ($what == 'header' && variable('node') == 'index') {
+		$vars['optional-slider'] = getSnippet('spa-slider');
+	}
+
+	return $vars;
+}
+
+function after_footer_assets() {
+	if (variable('node') == 'index') {
+		echo getSnippet('slider-footer');
+	}
+}
+
+function site_before_render() {
+	$section = variable('section');
+	$node = variable('node');
+
+	if (!$section || $section == $node) return;
+
+	$isSpecial = in_array($node, variable('special-nodes'));
+	DEFINE('NODEPATH', SITEPATH . '/' . variable('section') . '/' . $node);
+	variables([
+		assetKey(NODEASSETS) => fileUrl('assets/nodes/'),
+		'nodeSiteName' => humanize($node),
+		'nodeSafeName' => $isSpecial ? $section . '-' . $node : 'default',
+		'submenu-at-node' => true,
+	]);
+}
---
+++ content/index.php
@@ -0,0 +1,49 @@
+<!-- Niche Demos -->
+<link rel="stylesheet" href="https://canvastemplate.com/demos/spa/spa.css">
+
+<?php
+
+// *****
+
+$homeVars = [
+	'about-text' => 'SOMETHING HERE',
+	'cta-w-100-link' => '#todo',
+	'cta-w-100' => 'apply for the unique VidyAntara member[friend]ship',
+	'suggestions-text' => 'MORE IN THIS SPACE',
+];
+
+// *****
+
+$itemTemplate = disk_file_get_contents(SITEPATH . '/data/templates/feature.html');
+$sheet = getSheet('features', false);
+$items = [];
+
+foreach ($sheet->rows as $item) {
+	$item = rowToObject($item, $sheet);
+	$item['link'] = pageUrl($item['link']);
+	$items[] = replaceItems($itemTemplate, $item, '%');
+}
+
+$homeVars['featureHtml'] = implode(NEWLINES2, $items);
+
+// *****
+
+$itemTemplate = disk_file_get_contents(SITEPATH . '/data/templates/suggestion.html');
+$sheet = getSheet('suggestions', 'ix');
+
+foreach ($sheet->group as $ix => $rows) {
+	$homeVars['suggestion-' . $ix] = $sheet->getValue($rows[0], 'group');
+	$items = [];
+	foreach ($rows as $item) {
+		$item = rowToObject($item, $sheet);
+		$item['link'] = pageUrl($item['link']);
+		$items[] = replaceItems($itemTemplate, $item, '%');
+	}
+	$homeVars['suggestions-' . $ix] = implode(NEWLINES2, $items);;
+}
+
+// *****
+
+$itemTemplate = disk_file_get_contents(SITEPATH . '/data/templates/home.html');
+
+echo replaceItems($itemTemplate, $homeVars, '%');
